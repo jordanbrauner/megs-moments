f531353793c7cdc011ccd4937e093de417fd667e
Jordan Brauner <jordan.brauner@gmail.com>
2016-03-24 13:23:52 -0400

    Updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
   "use strict";
 
+  $('#scroll-to-about').click(function(){
+      $('html, body').animate({
+          scrollTop: $("#section-about").offset().top
+      }, 400);
+  });
+
+  $('#scroll-to-gallery').click(function(){
+      $('html, body').animate({
+          scrollTop: $("#section-gallery").offset().top
+      }, 400);
+  });
+
 
 });
